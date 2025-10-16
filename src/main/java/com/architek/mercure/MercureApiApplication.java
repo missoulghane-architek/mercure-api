a7ba342b0db20f0b3945019d5ca83ee9ab924612
Morad ISSOULGHANE <morad@MacBook-Air-de-Morad.local>
2025-10-16 16:44:59 +0200

    Initial commit

---
+++ src/main/java/com/architek/mercure/MercureApiApplication.java
@@ -0,0 +1,13 @@
+package com.architek.mercure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercureApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercureApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/architek/mercure/MercureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.architek.mercure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercureApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
